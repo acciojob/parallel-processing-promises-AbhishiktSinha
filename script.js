b7200bfb3d01940f86a98704d73b3382cbc36501
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-26 08:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,19 +6,37 @@ const images = [
     { url: "https://picsum.photos/id/237/200/300" },
     { url: "https://picsum.photos/id/238/200/300" },
     { url: "https://picsum.photos/id/239/200/300" },
+
 ];
 
 function createImageLoadPromise(index) {
-    return new Promise(function promCallback(resolve, reject) {
+    const imagePromise = new Promise(function promCallback(resolve, reject) {
 
         // perform synchronous task of loading image
         const image = document.createElement('img');
         image.src = images[index++].url;
         image.alt = 'Image';
 
-        resolve(image);
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+
+        image.addEventListener('error', () => {
+            reject(`Failed to load image:${image.src}`);
+        })
+
 
     });
+
+    imagePromise
+        .then((result) => {
+            console.log('loaded: ', result.src);
+        })
+        .catch((reason) => {
+            console.log(reason);
+        });;
+
+    return imagePromise;
 }
 
 btn.addEventListener('click', () => {
@@ -30,13 +48,15 @@ btn.addEventListener('click', () => {
         imagePromiseArray.push(createImageLoadPromise(index));
     });
 
-    Promise.all(imagePromiseArray)
+    const combinedImagePromise = Promise.all(imagePromiseArray);
+    
+    combinedImagePromise
         .then((resultArray) => {
             resultArray.forEach((imageElement) => {
                 output.appendChild(imageElement);
             });
         })
         .catch((reason) => {
-            console.error('ERROR: Error in downloading image', reason)
+            console.info(reason);
         })
 })
