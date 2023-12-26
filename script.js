15a41b1f44ec4f8aeac88430fef79be6e0492faa
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-26 07:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//your JS code here. If required.
+
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function createImageLoadPromise(index) {
+    return new Promise(function promCallback(resolve, reject) {
+
+        // perform synchronous task of loading image
+        const image = document.createElement('img');
+        image.src = images[index++].url;
+        image.alt = 'Image';
+
+        resolve(image);
+
+    });
+}
+
+btn.addEventListener('click', () => {
+
+    output.innerHTML = '';
+
+    const imagePromiseArray = [];
+    images.forEach((element, index) => {
+        imagePromiseArray.push(createImageLoadPromise(index));
+    });
+
+    Promise.all(imagePromiseArray)
+        .then((resultArray) => {
+            resultArray.forEach((imageElement) => {
+                output.appendChild(imageElement);
+            });
+        })
+        .catch((reason) => {
+            console.error('ERROR: Error in downloading image', reason)
+        })
+})
